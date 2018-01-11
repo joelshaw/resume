821e7c303c9921438e5cfdc0968803ba3dfc1bd7
Joel Shaw <jshaw@rvadesignsolutions.com>
2018-01-10 22:10:11 -0500

    Install & Configure CSS Loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from './styles/Styles.css';
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
