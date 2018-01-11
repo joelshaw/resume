d177f8eb00a4824c1afcf9e730a4a5bf079012ac
Joel Shaw <jshaw@rvadesignsolutions.com>
2018-01-10 22:01:25 -0500

    Create base folder/file structure, Created test React component files.

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render(){
+        return (
+            <div class="test" style={{textAlign: 'center'}}>
+                <h2>Resume</h2>
+            </div>
+        );
+    }
+}
